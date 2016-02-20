53ab3607f557261338a605f94338030f482d88a1
Andra <andrazaharia@ymail.com>
2016-02-20 18:38:39 +0000

    sentiment function

--- koala/sentiment.py
+++ koala/sentiment.py
@@ -1,16 +1,30 @@
-import requests, json
+import requests
 
-url = "http://text-processing.com/api/sentiment/"
+def sentimentAnalysis(txt):
+    # using the Sentiment Analysis API
+    url = "http://text-processing.com/api/sentiment/"
 
-txt = "text=great"
+    posNo = 0
+    negNo = 0
 
-req = requests.post(url,data=txt)
-print
-print req.text
-print
-(prob, label) = req.text.split('},')
-(lb, value) = label.split(": ")
-val = value[1:4]
+    for word in txt:
+        req = requests.post(url,data="text="+word)
+    #    print
+    #    print req.text
+    #    print
+        (prob, label) = req.text.split('},')
+        (lb, value) = label.split(": ")
+        # final value is pos, neg or neu
+        val = value[1:4]
 
-# final value is pos, neg or neu
-print val
+        if (val == "pos"):
+            posNo += 1
+        elif (val == "neg"):
+            negNo += 1
+
+    if (posNo >= negNo):
+        return "HAPPYYYYY"
+    else:
+        return "ANGRYYYYYY"
+
+print sentimentAnalysis(["amazing", "great", "shit"])

66c42f4da9b4a03ee6e064a9a1a1c52a71c508d6
comRamona <com.ramona@yahoo.com>
2016-02-21 00:17:18 +0000

    why do we cry and break things sodepressing heartbreak please kill me poor poop

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,7 +1,10 @@
 import requests
 import requests_cache
 import json
-
+import nltk
+import random
+import unirest
+import sentiments
 from flask import Flask, render_template, request, jsonify
 from forms import LoginForm
 
@@ -32,26 +35,42 @@ def hello():
     name=request.form['name']
     url2="https://api.github.com/users/"+name+"/repos"
     ram='comRamona'
-    auth='00abf00f9ba10bd2f2187aa273b8b8cfa940fcb3'
+    auth= 'd3fd78a853bda3a636ee51e655f2cc6476a37498'
     repos = requests.get(url2,auth=(ram,auth)).json()
-    commits=[]
+    words=""
     for repo in repos:
         x=repo['name']
-        commits+=x
         f.write(x)
     for repo in repos:
         repo_name=repo['name']
-        commits+=repo_name
         f.write("NAME: "+repo_name+"\n\n\n")
         commit_url="https://api.github.com/repos/"+name+"/"+repo_name+"/commits"
         all=requests.get(commit_url,auth=(ram,auth)).json()
         for a in all:
             if(type(a) is dict):
-                x=unicode(a['commit']['message'])
-                commits+=x
+                x=str(a['commit']['message'])
+                y=nltk.tokenize.regexp_tokenize(x, r'\w+')
+                words=words+" ".join(y)
                 f.write(x)
     f.close()
-    return render_template('commits.html', name=name,commits=commits)
+    k=len(words)
+    j=50
+    if(k<30):
+        j=k
+    words=random.sample(words, j)
+    response = unirest.post("https://atrilla-nlptools.p.mashape.com/?app_key=",
+    headers={
+    "X-Mashape-Key": "TfcYFWowjOmshyyeFxy2NY96rhlAp19z6pwjsnexQLc1Wj19WC",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Accept": "text/plain"
+    },
+    params={
+    "service": "sentiment_news",
+    "text": words
+    }
+    )
+
+    return render_template('commits.html', name=name,res=res)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ flask/app/sentiments.py
@@ -0,0 +1,28 @@
+import requests
+
+def sentimentAnalysis(txt):
+    # using the Sentiment Analysis API
+    url = "http://text-processing.com/api/sentiment/"
+
+    posNo = 0
+    negNo = 0
+
+    for word in txt:
+        req = requests.post(url,data="text="+word)
+    #    print
+    #    print req.text
+    #    print
+        (prob, label) = req.text.split('},')
+        (lb, value) = label.split(": ")
+        # final value is pos, neg or neu
+        val = value[1:4]
+
+        if (val == "pos"):
+            posNo += 1
+        elif (val == "neg"):
+            negNo += 1
+
+    if (posNo >= negNo):
+        return "HAPPYYYYY"
+    else:
+        return "ANGRYYYYYY"
